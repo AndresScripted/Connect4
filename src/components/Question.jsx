6742f7969b1a2b50742ebfe1c8f3cac66f0e87cf
Andres Huitzil <jib@Andress-MacBook-Pro.local>
2020-02-05 18:12:14 -0800

    can pull data from firebase

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import '../css/Question.css';
 import {buildFirebase} from '../clients/firebase.js';
 // import components
-var database = buildFirebase();
-var databaseRef = database.ref("/questions");
-databaseRef.once("value").then(function(data) {
- const questions = data.val();
+
 // Do something with the questions
-console.log(questions);
-});
+
 class Question extends Component {
 constructor(props){
   super(props);
@@ -21,25 +17,25 @@ constructor(props){
 
 
   render() {
+    var database = buildFirebase();
+    var databaseRef = database.ref("/questions");
+    databaseRef.once("value").then(function(data) {
+      const questions = data.val();
+      console.log(questions);
+    });
     return (
       <div>
          <h1> {this.props.question} </h1>
          <ul id="container">
-            <button class={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
-            <button class={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
-            <button class={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
-            <button class={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
+            <button className={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
+            <button className={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
+            <button className={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
+            <button className={this.state.ask} onClick={() => this.WhenClick()}> {this.props.Question} </button>
          </ul>
       </div>
     );
   }
 }
-class QuestionConstructor {
-    constructor (color, shape, correct) {
-      this.color = color;
-      this.shape = shape;
-      this.correct = correct;
-    }
-}
+
 
 export default Question;
