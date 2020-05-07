eac5059adb9b5ac9d5266a1848e7462d789aaa66
Andres Huitzil <jib@Andress-MacBook-Pro.local>
2020-05-06 18:42:03 -0700

    deleted old stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,28 +6,13 @@ import Question from './Question.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-      // factNumber: 0
-    // }
-    // this.handleClick = this.handleClick.bind(this);
-    //console.log(keys) // [apple, orange, pear]
-      this.state = {
-         factNumber: 0
-       }
-       this.handleClick = this.handleClick.bind(this);
   }
-  handleClick() {
-    this.setState({factNumber:this.state.factNumber+1});
-  }
+    
 
   render() {
     return (
       <div className="app">
-        <div id="title">Kahoot but more Fustrating! :D</div>
-        <div id="questionbox">
-          <Question questionText={this.props.Questions[this.state.factNumber].question_text} Choices={this.props.Questions[this.state.factNumber].choices} CorrectIndex={this.props.Questions[this.state.factNumber].correct_choice_index}/>
-          </div>
-      <button className="nextButton" onClick={this.handleClick}>Next Question :D</button>
+        
       </div>
 
     );
